372f035179821a1ec878f751e0b9b837c70f48a8
Camm67 <130731742+Camm67@users.noreply.github.com>
2026-05-28 21:00:54 -0500

    Refactor main.c to include new LVGL modules

--- example/ESP-IDF-5.3.2/ESP32-S3-Touch-LCD-1.46-Test/main/main.c
+++ example/ESP-IDF-5.3.2/ESP32-S3-Touch-LCD-1.46-Test/main/main.c
@@ -4,7 +4,8 @@
 #include "SD_MMC.h"
 #include "Wireless.h"
 #include "TCA9554PWR.h"
-#include "LVGL_Example.h"
+#include "LVGL_Driver.h"
+#include "LVGL_BubbleLevel.h"
 #include "BAT_Driver.h"
 #include "PWR_Key.h"
 #include "PCM5101.h"
@@ -13,8 +14,7 @@
 void Driver_Loop(void *parameter)
 {
     Wireless_Init();
-    while(1)
-    {
+    while (1) {
         QMI8658_Loop();
         PCF85063_Loop();
         BAT_Get_Volts();
@@ -23,47 +23,39 @@ void Driver_Loop(void *parameter)
     }
     vTaskDelete(NULL);
 }
+
 void Driver_Init(void)
 {
     PWR_Init();
     BAT_Init();
     I2C_Init();
-    EXIO_Init();                    // Example Initialize EXIO
+    EXIO_Init();
     Flash_Searching();
     PCF85063_Init();
     QMI8658_Init();
     xTaskCreatePinnedToCore(
-        Driver_Loop, 
+        Driver_Loop,
         "Other Driver task",
-        4096, 
-        NULL, 
-        3, 
-        NULL, 
+        4096,
+        NULL,
+        3,
+        NULL,
         0);
 }
+
 void app_main(void)
 {
     Driver_Init();
-
     SD_Init();
     LCD_Init();
     Audio_Init();
     MIC_Speech_init();
-    // Play_Music("/sdcard","AAA.mp3");
-    LVGL_Init();   // returns the screen object
+    LVGL_Init();
 
-// /********************* Demo *********************/
-    Lvgl_Example1();
-    // lv_demo_widgets();
-    // lv_demo_keypad_encoder();
-    // lv_demo_benchmark();
-    // lv_demo_stress();
-    // lv_demo_music();
+    BubbleLevel_Show();
 
     while (1) {
-        // raise the task priority of LVGL and/or reduce the handler period can improve the performance
         vTaskDelay(pdMS_TO_TICKS(10));
-        // The task running lv_timer_handler should have lower priority than that running `lv_tick_inc`
         lv_timer_handler();
     }
 }
